60a3090f79b9c593244ec766a727a4688f8327f2
Domisa <molsbarger.daniel@gmail.com>
2026-07-22 20:52:27 -0500

    feat: added Board component

---
+++ src/components/Board.tsx
@@ -0,0 +1,25 @@
+import type { Task } from '../types'
+import Column from './Column'
+
+interface BoardProps {
+    tasks: Task[]
+}
+//temporary hard limit to the current columns. No customization just yet
+const DEFAULT_COLUMNS = ['To Do', 'In Progress', 'In Review', 'Done']
+
+function Board({ tasks }: BoardProps) {
+    //more temporary styling. Might use Tailwind
+    return (
+        <div style ={{ display: 'flex', gap: '1rem'  }}>
+            {DEFAULT_COLUMNS.map((status) => (
+                <Column
+                    key={status}
+                    title={status}
+                    tasks={tasks.filter((task) => task.status === status)}
+            />
+        ))}
+        </div>
+    )
+}
+
+export default Board
